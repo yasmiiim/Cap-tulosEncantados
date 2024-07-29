4a2a350d02cfdae2259ce93f8185bfa925d3749d
yasmim <suzzyasmim2016@gmail.com>
2024-07-28 21:56:42 -0300

    pulo duplo

--- CapitulosEncantados/Assets/Character.cs
+++ CapitulosEncantados/Assets/Character.cs
@@ -2,34 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Character : MonoBehaviour
 {
-    public float Speed; 
+    public float Speed;
     public float Jumpforce;
+    public int maxJumpCount = 2; // Número máximo de saltos (double jump é 2 saltos)
 
-    public bool isJumping;
+    public int jumpCount; // Contador de saltos restantes
+    public bool isGrounded; // Verifica se o personagem está no chão
 
     private Rigidbody2D rig;
-
-
 
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
-
+        jumpCount = maxJumpCount; // Inicializa o número de saltos
     }
 
     void Update()
     {
-
         Move();
         Jump();
-
     }
-
-
-
-
 
     void Move()
     {
@@ -38,30 +33,22 @@ public class Character : MonoBehaviour
 
         float inputAxis = Input.GetAxis("Horizontal");
 
-        if(inputAxis > 0)
+        if (inputAxis > 0)
         {
             transform.eulerAngles = new Vector2(0f, 0f);
         }
-
-        if(inputAxis < 0)
+        else if (inputAxis < 0)
         {
             transform.eulerAngles = new Vector2(0f, 180f);
         }
-
-
     }
 
     void Jump()
     {
-        if(Input.GetButtonDown("Jump") && !isJumping)
-    {
-
-        rig.AddForce(new Vector2(0f, Jumpforce), ForceMode2D.Impulse);
+        if (Input.GetButtonDown("Jump") && jumpCount > 0)
+        {
+            rig.AddForce(new Vector2(0f, Jumpforce), ForceMode2D.Impulse);
+            jumpCount--; // Decrementa o contador de saltos
+        }
     }
-
-    }
-
-    
 }
-
-
--- CapitulosEncantados/Assets/Grounded.cs
+++ CapitulosEncantados/Assets/Grounded.cs
@@ -2,31 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Grounded : MonoBehaviour
 {
     private Character player; 
 
     void Start()
     {
-       
         player = gameObject.transform.parent.gameObject.GetComponent<Character>();
     }
 
     void OnCollisionEnter2D(Collision2D collider)
     {
-       
-        if (collider.gameObject.layer == 8)
+        // Verifica se o personagem está colidindo com o chão (camada 8)
+        if (collider.gameObject.layer == 8) 
         {
-            player.isJumping = false;
+            player.isGrounded = true; // O personagem está no chão
+            player.jumpCount = player.maxJumpCount; // Restaura o número de saltos
         }
     }
 
     void OnCollisionExit2D(Collision2D collider)
     {
-        
-        if (collider.gameObject.layer == 8)
+        // Verifica se o personagem saiu do chão (camada 8)
+        if (collider.gameObject.layer == 8) 
         {
-            player.isJumping = true;
+            player.isGrounded = false; // O personagem saiu do chão
         }
     }
 }
+
