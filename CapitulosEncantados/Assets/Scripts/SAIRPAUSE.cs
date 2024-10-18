c0c5ff47a619d463252ef50950688160cd2b97be
yasmim <suzzyasmim2016@gmail.com>
2024-10-17 21:00:23 -0300

    BOTAO DE SAIR NO PAUSE

---
+++ CapitulosEncantados/Assets/Scripts/SAIRPAUSE.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SAIRPAUSE : MonoBehaviour
+{
+    
+    public void Sair()
+    {
+        Debug.Log("Saiu do jogo");
+        Application.Quit();
+    }
+}
