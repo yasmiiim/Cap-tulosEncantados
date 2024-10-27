e276f3a5d6357bc621456a1e810917687ffccbf1
yasmim <suzzyasmim2016@gmail.com>
2024-10-27 17:19:41 -0300

    fumacinha

---
+++ CapitulosEncantados/Assets/ParticleController.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleController : MonoBehaviour
+{
+   [SerializeField] private ParticleSystem movementParticle;
+
+   [Range(0, 10)] 
+   [SerializeField] private int occurAfterVelocity;
+
+   [Range(0, 0.2f)] [SerializeField] private float dustFormationPeriod;
+
+   [SerializeField] private Rigidbody2D playerRb;
+   
+   float counter;
+
+   bool isOnGround;
+
+   private void Update()
+   {
+      counter += Time.deltaTime;
+
+      if (isOnGround && Mathf.Abs(playerRb.velocity.x) > occurAfterVelocity)
+      {
+         if (counter > dustFormationPeriod)
+         {
+            movementParticle.Play();
+            counter = 0;
+         }
+      }
+   }
+
+   private void OnTriggerEnter2D(Collider2D collision)
+   {
+      if (collision.CompareTag("chao"))
+      {
+         isOnGround = true;
+      }
+   }
+   
+   private void OnTriggerExit2D(Collider2D collision)
+   {
+      if (collision.CompareTag("chao"))
+      {
+         isOnGround = false;
+      }
+   }
+}
