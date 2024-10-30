4c3c73ab253ed3bdb56b48c59c3212b7bbbee08d
yasmim <suzzyasmim2016@gmail.com>
2024-10-30 09:58:58 -0300

    animacao cogumelo e poçao

--- CapitulosEncantados/Assets/Scripts/cenario/cogumelo.cs
+++ CapitulosEncantados/Assets/Scripts/cenario/cogumelo.cs
@@ -6,20 +6,35 @@ using UnityEngine;
 public class cogumelo : MonoBehaviour
 {
     public float bounce = 20f;
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            // Verifica se o contato é na parte de cima do cogumelo
             foreach (ContactPoint2D contact in other.contacts)
             {
-                if (contact.normal.y < 0) // A colisão deve ocorrer de cima para baixo
+                if (contact.normal.y < 0)
                 {
                     other.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounce, ForceMode2D.Impulse);
-                    break; // Sai do loop após aplicar a força
+                    
+                    animator.SetBool("isJumping", true);
+
+                    StartCoroutine(ResetIsJumping());
+                    break;
                 }
             }
         }
+    }
+
+    private IEnumerator ResetIsJumping()
+    {
+        yield return new WaitForSeconds(0.1f);
+        animator.SetBool("isJumping", false);
     }
 }
