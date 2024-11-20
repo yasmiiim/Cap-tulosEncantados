ecfada8201775952ee0956dce2024411d141a376
yasmiiim <129852221+yasmiiim@users.noreply.github.com>
2024-11-20 13:39:23 -0300

    ajuste pedra

---
+++ CapitulosEncantados/Assets/pedraTuto.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pedraTuto : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            AudioObserver.OnPlaySfxEvent("coletar");
+            Destroy(gameObject);
+
+        }
+    }
+}
