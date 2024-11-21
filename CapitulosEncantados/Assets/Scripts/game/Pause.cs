aa1ec2b87a48b8bdd8fd42b9032094adafae068f
yasmiiim <129852221+yasmiiim@users.noreply.github.com>
2024-11-21 17:41:51 -0300

    pauseeee

--- CapitulosEncantados/Assets/Scripts/game/Pause.cs
+++ CapitulosEncantados/Assets/Scripts/game/Pause.cs
@@ -4,22 +4,32 @@ using UnityEngine;
 
 public class Pause : MonoBehaviour
 {
-    public Transform pauseMenu;
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            if (pauseMenu.gameObject.activeSelf)
-            {
-                pauseMenu.gameObject.SetActive(false);
-                Time.timeScale = 1;
-            }
-            
-            else
-            {
-                pauseMenu.gameObject.SetActive(true);
-                Time.timeScale = 0;
-            }
-        }
-    }
+  public Transform pauseMenu;
+  
+      void Update()
+      {
+          if (Input.GetKeyDown(KeyCode.Escape))
+          {
+              if (pauseMenu.gameObject.activeSelf)
+              {
+                  ResumeGame();
+              }
+              else
+              {
+                  PauseGame();
+              }
+          }
+      }
+  
+      public void ResumeGame()
+      {
+          pauseMenu.gameObject.SetActive(false);
+          Time.timeScale = 1;
+      }
+  
+      private void PauseGame()
+      {
+          pauseMenu.gameObject.SetActive(true);
+          Time.timeScale = 0;
+      }
 }
