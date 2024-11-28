9e7dfdc2be174410cd66b81a0e3a4e725628f75e
yasmiiim <129852221+yasmiiim@users.noreply.github.com>
2024-11-28 17:45:16 -0300

    .

--- CapitulosEncantados/Assets/Scripts/Player/VidaPlayer.cs
+++ CapitulosEncantados/Assets/Scripts/Player/VidaPlayer.cs
@@ -127,6 +127,10 @@ public class VidaPlayer : MonoBehaviour
         {
             Die();
         }
+        else if (collision.tag == "abelha")
+        {
+            ReceberDano();
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D coli)
