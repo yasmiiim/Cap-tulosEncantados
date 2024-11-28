284cec230279d8671441ff32fe7cbe6d680e7a7a
yasmiiim <129852221+yasmiiim@users.noreply.github.com>
2024-11-27 22:08:02 -0300

    ;

--- CapitulosEncantados/Assets/Scripts/game/Pause.cs
+++ CapitulosEncantados/Assets/Scripts/game/Pause.cs
@@ -1,35 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; // Importar o SceneManager
 
 public class Pause : MonoBehaviour
 {
-  public Transform pauseMenu;
-  
-      void Update()
-      {
-          if (Input.GetKeyDown(KeyCode.Escape))
-          {
-              if (pauseMenu.gameObject.activeSelf)
-              {
-                  ResumeGame();
-              }
-              else
-              {
-                  PauseGame();
-              }
-          }
-      }
-  
-      public void ResumeGame()
-      {
-          pauseMenu.gameObject.SetActive(false);
-          Time.timeScale = 1;
-      }
-  
-      private void PauseGame()
-      {
-          pauseMenu.gameObject.SetActive(true);
-          Time.timeScale = 0;
-      }
+    public Transform pauseMenu;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pauseMenu.gameObject.activeSelf)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    private void PauseGame()
+    {
+        pauseMenu.gameObject.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    // Método para carregar o menu inicial
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1; // Certificar que o tempo volta ao normal antes de trocar de cena
+        SceneManager.LoadScene("Menu"); 
+    }
 }
--- CapitulosEncantados/Assets/Scripts/game/SAIRPAUSE.cs
+++ CapitulosEncantados/Assets/Scripts/game/SAIRPAUSE.cs
@@ -1,13 +1,14 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class SAIRPAUSE : MonoBehaviour
 {
-    
-    public void Sair()
+    public string menuSceneName = "Menu";
+    public void voltar()
     {
-        Debug.Log("Saiu do jogo");
-        Application.Quit();
+        SceneManager.LoadScene(menuSceneName);
     }
-}
+}*/
